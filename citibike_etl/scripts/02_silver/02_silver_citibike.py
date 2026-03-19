37e63cd0dfb249d5a178fcc5fd659c2f4279929f
Ben Deininger <bendeininger1@gmail.com>
2026-03-19 16:37:23 -0400

    fixed import

--- citibike_etl/scripts/02_silver/02_silver_citibike.py
+++ citibike_etl/scripts/02_silver/02_silver_citibike.py
@@ -6,8 +6,8 @@ project_root = os.path.abspath(os.path.join(current_dir, "..","..",".."))
 
 sys.path.append(project_root)
 
-from citibike.citibike_utils import get_trip_duration_mins
-from utils.datetime_utils import timestamp_to_date_col
+from src.citibike.citibike_utils import get_trip_duration_mins
+from src.utils.datetime_utils import timestamp_to_date_col
 from pyspark.sql.functions import create_map, lit
 
 pipeline_id = sys.argv[1]
